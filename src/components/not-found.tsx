7e69ba85118ae71196510d4284e49a3aebec83ae
TromiUss <thepeaceofmind@yandex.ru>
2025-07-14 19:54:56 +0300

    not-found-page modif

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,13 +1,28 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../../public/css/not-found.css'
 
 function NotFoundScreen(): JSX.Element {
   return (
     <section className="main-page">
       <section className="main-page__screen">
-        <h1>404. Page not found</h1>
-        <Link to="/">Вернуться на главную</Link>
-      </section>
+      <h1 className="not-found-header">
+        404
+        <span aria-hidden="true">404</span>
+      </h1>
+      <div className="cloak__wrapper">
+        <div className="cloak__container">
+          <div className="cloak"></div>
+        </div>
+      </div>
+      <div className="info">
+        <p className="not-found-text">
+          We're fairly sure that page used to be here, but seems to have gone
+          missing. We do apologise on it's behalf.
+        </p>
+      </div>
+      <Link to="/">Вернуться на главную</Link>
     </section>
+    </section >
   );
 }
 
