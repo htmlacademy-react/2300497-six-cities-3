45dac63628be185e313b224b6deb643bdbd7fbef
TromiUss <121058447+TromiUss@users.noreply.github.com>
2025-07-29 13:23:31 +0300

    fav size img

--- src/components/cities-cards.tsx
+++ src/components/cities-cards.tsx
@@ -12,6 +12,10 @@ type CitiesCardProps = {
   isActive: boolean;
   onMouseEnter: MouseEventHandler<HTMLDivElement>;
   onMouseLeave: MouseEventHandler<HTMLDivElement>;
+  wrapperClassName?: 'cities__card' | 'favorites__card';
+  imageWrapperClassName?: 'cities__image-wrapper' | 'favorites__image-wrapper';
+  imageWidth?: number;
+  imageHeight?: number;
 };
 
 function CitiesCard({
@@ -19,6 +23,10 @@ function CitiesCard({
   isActive,
   onMouseEnter,
   onMouseLeave,
+  wrapperClassName = 'cities__card',
+  imageWrapperClassName = 'cities__image-wrapper',
+  imageWidth = 260,
+  imageHeight = 200,
 }: CitiesCardProps) {
   const dispatch = useDispatch<AppDispatch>();
   const isAuthorized = useSelector(selectIsAuthorized);
@@ -36,62 +44,60 @@ function CitiesCard({
   };
 
   return (
-    <div
-      className={`cities__place-card place-card ${isActive ? 'active' : ''}`}
+    <article
+      className={`place-card ${wrapperClassName} ${isActive ? 'active' : ''}`}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      <article className="cities__card place-card">
-        {offer.isPremium && (
-          <div className="place-card__mark">
-            <span>Premium</span>
-          </div>
-        )}
-        <div className="cities__image-wrapper place-card__image-wrapper">
-          <img
-            className="place-card__image"
-            src={offer.previewImage}
-            width="260"
-            height="200"
-            alt="Place image"
-          />
+      {offer.isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
         </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{offer.price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button
-              className={`place-card__bookmark-button button ${
-                offer.isFavorite ? 'place-card__bookmark-button--active' : ''
-              }`}
-              type="button"
-              onClick={handleFavoriteClick}
-            >
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
+      )}
+      <div className={`place-card__image-wrapper ${imageWrapperClassName}`}>
+        <img
+          className="place-card__image"
+          width={imageWidth}
+          height={imageHeight}
+          src={offer.previewImage}
+          alt="Place image"
+        />
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{ width: `${(offer.rating / 5) * 100}%` }}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <Link to={`/offer/${offer.id}`}>
-            <h2 className="place-card__name">
-              <div>{offer.title}</div>
-            </h2>
-          </Link>
-          <p className="place-card__type">
-            {offer.type.charAt(0).toUpperCase() + offer.type.slice(1)}
-          </p>
+          <button
+            className={`place-card__bookmark-button button ${
+              offer.isFavorite ? 'place-card__bookmark-button--active' : ''
+            }`}
+            type="button"
+            onClick={handleFavoriteClick}
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
         </div>
-      </article>
-    </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: `${(offer.rating / 5) * 100}%` }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <Link to={`/offer/${offer.id}`}>
+          <h2 className="place-card__name">
+            <div>{offer.title}</div>
+          </h2>
+        </Link>
+        <p className="place-card__type">
+          {offer.type.charAt(0).toUpperCase() + offer.type.slice(1)}
+        </p>
+      </div>
+    </article>
   );
 }
 
